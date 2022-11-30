6f1db057a708fb27cf5e23851495e0f2aeeae7a4
fiyanoush <97440412+fiyanoush@users.noreply.github.com>
2022-12-01 01:39:55 +0530

    Add files via upload

--- Bird_Classification.py
+++ Bird_Classification.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from tensorflow.keras.image import load_img,img_to_array
+from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
 from keras.models import load_model
 
